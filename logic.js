28cb351c4e74dd878d05ae2313cc392ea4b9b0e9
kaushikDev <piyush.kaushik02@gmail.com>
2020-04-22 14:11:42 +0530

    entire logic redone

--- logic.js
+++ logic.js
@@ -1,185 +1,200 @@
-"use strict";
-let display = document.querySelector("#display");
-let xPowerY =  document.querySelector("#xPowerY");
-let eq = document.querySelector("#eq");
-let tempVal;
-
-// Function key : This function puts the value of a numeric or arithmetic operator key pressed in to the display area
-function onLoad() {
-  display.value = "0";
-  xPowerY.disabled = true;
-}
-
-//  Separate function for key '^'
-function carat(val) {
-  tempVal = display.value;
-  display.value += val;
-  xPowerY.disabled = false;
-  xPowerY.style.backgroundColor = "#2693e0";
-  eq.disabled = true;
-  eq.style.backgroundColor = "gray";
+class Calculator {
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+    this.readyToReset = false;
+    this.scientific = false;
+    this.clear();
   }
 
-
-// Function key : This function puts the value of a numeric or arithmetic operator key pressed in to the display area
-function key(val) {
-  tempVal = display.value;
-
-  if (tempVal === "0") {
-    display.value = "";
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
   }
-  switch (val) {
-    case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0':
-      display.value += val;
+
+  delete() {
+      this.currentOperand =  this.currentOperand.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (
+      (number === "." && this.currentOperand.includes(".")) ||
+      (number === "0" && this.currentOperand[0] === "0") ||
+      (number === "00" && this.currentOperand === "")
+    )
+      return;
+   
+      this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  scientificOperation(scientificOperator){
+    this.clear();
+    this.currentOperand = scientificOperator+"[" ;
+   
+   }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let result;
+    if(this.scientific === true) {
+    //  console.log(this.currentOperand);
+      let scientificOperator = this.currentOperand.split('[');
+     // console.log(scientificOperator);
+
+      switch(scientificOperator[0]){
+        case "sin":
+        result = Math.sin(scientificOperator[1]);
+        break;
+        case "cos":
+          result = Math.cos(scientificOperator[1]);
+        break;
+        case "tan":
+          result = Math.tan(scientificOperator[1]);
+        break;
+        case "Log(2)":
+          result = Math.log2(scientificOperator[1]);
+        break;
+        case "Log(10)":
+          result = Math.log10(scientificOperator[1]);
+        break;
+        case "Fact(!)":
+          let n = scientificOperator[1];
+          function factorial(num){
+            if (num <= 1) {
+                return 1;
+            } else {
+                return num * factorial(num-1);
+            }
+        }
+        result = factorial(n);
+           
+        break;
+        case "√":
+          result = Math.sqrt(scientificOperator[1]);
+        break;
+        case "∛":
+          result = Math.cbrt(scientificOperator[1]);
+        break;
+        default:
+        return;
+
+      }
+
+    }
+else {
+  let previous = parseFloat(this.previousOperand);
+  let current = parseFloat(this.currentOperand);
+  if (isNaN(previous) || isNaN(current)) return;
+  switch (this.operation) {
+    case "+":
+      result = previous + current;
       break;
-    case '+': case '-': case '*': case '/': case '%': case '.': case '(': case ')':
-      tempVal;
-      tempVal = display.value;
-      if (tempVal = "") {
-        display.value = "";
-      }
-      else if (tempVal != '+' && tempVal != '-' && tempVal != '*' && tempVal != '/' && tempVal != '%' && tempVal != '.') {
-        display.value += val;
-      }
-      else {
-        display.value = tempVal;
-      }
+    case "-":
+      result = previous - current;
+      break;
+    case "*":
+      result = previous * current;
+      break;
+    case "÷":
+      result = previous / current;
+      break;
+    case "mod":
+      result = previous % current;
+      break;
+    case "^":
+      result = Math.pow(previous, current);
+      break;
+      case "%":
+        result = (previous/100) * current;
+        break;        
+    default:
+      return;
   }
 }
+ 
 
-// <!-- Function allClear : This function clears everything in the display area-->
-function allClear() {
-  display.value = "0";
-  xPowerY.style.backgroundColor = "transparent";
-}
-
-// <!-- This function clears the entry one by one, only if "0" is not displayed -->
-function pop() {
-  if (display.value === "0") {
-    //do nothing.
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = "";
+    this.readyToReset =  true;
+    this.scientific = false;
   }
-  else {
-    let dispArr = (display.value).split('');
-    dispArr.pop();
-    display.value = dispArr.join('');
-    if (dispArr.length === 0) {
-      onLoad();
+
+  updateDisplay() {
+
+  this.currentOperandText.innerText = this.currentOperand;
+    if(this.operation != null){
+        this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
     }
-  }
+   
+    else this.previousOperandText.innerText = "";
 
-}
-
-// <!-- This function evaluates the expression in the display area -->
-function ans() {
-  tempVal = display.value;
-  try {
-    if (!Number.isInteger(eval(tempVal))) {
-      display.value = eval(tempVal).toFixed(4);
-    }
-    else{
-      display.value = eval(tempVal);
-    }
-    
-
-  } catch (e) {
-    display.value = "Wrong Expression!";
-
-  }
-  finally {
-   console.log("Evaluated Expression : " + display.value);
-  }
-}
-
-
-
-// <!-- This function evaluates square root -->
-function Sqrt() {
-  tempVal = display.value;
-  display.value = Math.sqrt(tempVal).toFixed(4);
-}
-
-// <!-- This function evaluates sine -->
-function sin() {
-  tempVal = display.value;
-  display.value = Math.sin(tempVal).toFixed(4);
-}
-
-// <!-- This function evaluates cosine -->
-function cos() {
-  tempVal = display.value;
-  display.value = Math.cos(tempVal).toFixed(4);
-}
-
-// <!-- This function evaluates tangent -->
-function tan() {
-  tempVal = display.value;
-  display.value = Math.tan(tempVal).toFixed(4);
-}
-
-// <!-- This function evaluates log to the base 10 -->
-function log() {
-  tempVal = display.value;
-  display.value = (Math.log(tempVal) / Math.LN10).toFixed(4);
-}
-
-// <!-- This function evaluates factorial -->
-function fact() {
-  tempVal = Math.floor(display.value);
-  if (tempVal <= 1) {
-    alert("Choose a number greater than 2");
-  }
-  let k = tempVal;
-  while (tempVal != 1) {
-    tempVal -= 1;
-    k *= (tempVal);
-  }
-  display.value = k;
-}
-
-// <!-- This function evaluates pi -->
-function PI() {
-  tempVal = display.value;
-  display.value = Math.PI.toFixed(4);
-  }
-
-// <!-- This function evaluates the expression in the display area -->
-function POW() {
-  let tempArr = display.value.split('');
-  console.log(tempArr);
-  //New code for more than single digits in calculation
-  let loc;
-  for (i = 0; i < tempArr.length; i++) {
-    if (tempArr[i] === '^') {
-      loc = i;
-    }
-  }
-  let base = '';
-  let expo = '';
-  for (i = 0; i < loc; i++) {
-    base += tempArr[i];
-  }
-  for (i = (loc + 1); i < tempArr.length; i++) {
-    expo += tempArr[i];
-  }
-  console.log("base is  : " + base);
-  console.log("expo is  : " + expo);
-try{
-  if (!isNaN(base) && !isNaN(expo)) {
-    display.value = Math.pow(base, expo);
-  }
-  else {
-    display.value = "Enter like this : x^y";
-  }
-}
-catch(e){
-  display.value = e;
-}
-finally{
-  eq.disabled = false;
-  xPowerY.disabled = true;
-  eq.style.backgroundColor = "#2693e0";
-  xPowerY.style.backgroundColor = "transparent";
-}
   
-
+  }
 }
+
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const scientificButtons = document.querySelectorAll("[data-scientific]");
+const clearAllButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const equalsButton = document.querySelector("[data-equals]");
+
+const calc = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if(calc.previousOperand === "" &&
+    calc.currentOperand !== "" &&
+calc.readyToReset) {
+        calc.currentOperand = "";
+        calc.readyToReset = false;
+    }
+    calc.appendNumber(button.innerText);
+    calc.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calc.chooseOperation(button.innerText);
+    calc.updateDisplay();
+  });
+});
+
+
+scientificButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calc.scientificOperation(button.innerText);
+    calc.scientific= true;
+    calc.updateDisplay();
+  });
+});
+
+
+equalsButton.addEventListener("click", () => {
+  calc.compute();
+  calc.updateDisplay();
+});
+
+clearAllButton.addEventListener("click", () => {
+    calc.clear();
+    calc.updateDisplay();
+  });
+
+  deleteButton.addEventListener("click", () => {
+    calc.delete();
+    calc.updateDisplay();
+  }); 
